5ea81b04947ca780629f933ced7a235319e7a09e
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-11 00:14:37 +0300

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from article import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = "index"),
     path('about/', views.about, name = "about"),
+    path('blogs/',include("article.urls")),
 ]
 
 

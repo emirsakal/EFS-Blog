f5126085b32dc9b7e62d3854c334f39cdf735871
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-10 20:16:22 +0300

    navbar

--- article/views.py
+++ article/views.py
@@ -2,7 +2,10 @@ from http.client import HTTP_PORT
 from django.shortcuts import render,HttpResponse
 
 def index(request):
-    return render(request,"index.html")
+    context = {
+        "numbers" :[1,2,3,4,5,6]
+    }
+    return render(request,"index.html",context)
 
 def about(request):
     return render(request,"about.html")

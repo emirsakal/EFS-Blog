e7cdcc9963e447a7db32b307b67fc5956de82584
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-11 03:58:11 +0300

    register-form

--- user/views.py
+++ user/views.py
@@ -6,30 +6,24 @@ from django.contrib.auth import login
 
 def register(request):
     
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
+    form = RegisterForm(request.POST or None)
 
-            newUser = User(username = username)
-            newUser.set_password(password)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
 
-            newUser.save()
-            login(request,newUser)
+        newUser = User(username = username)
+        newUser.set_password(password)
 
-            return redirect("index")
-        context = {
+        newUser.save()
+        login(request,newUser)
+
+        return redirect("index")
+    context = {
             "form" : form
         }
-        return render(request,"register.html",context)
+    return render(request,"register.html",context)
 
-    else:
-        form = RegisterForm()
-        context = {
-            "form" : form
-        }
-        return render(request,"register.html",context)
 
 def loginUser(request):
     return render(request,"login.html")

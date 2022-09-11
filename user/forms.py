685c7ddb249646b3078e489f8a79c264b189cbf6
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-11 13:22:50 +0300

    commit

--- user/forms.py
+++ user/forms.py
@@ -4,9 +4,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(label = "Username")
     password = forms.CharField(label = "Password",widget=forms.PasswordInput)
 
-    
-    
-
 class RegisterForm(forms.Form):
 
     username = forms.CharField(max_length=50,label = "Username")

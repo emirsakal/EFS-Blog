7ee31dd22ffbf34f6aac0d8460865e887288dcf3
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-12 15:13:31 +0300

    add blog

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from http.client import HTTP_PORT
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .forms import ArticleForm
+from django.contrib import messages
 
 def index(request):
     context = {
@@ -15,6 +16,14 @@ def dashboard(request):
     return render(request,"dashboard.html")
 
 def addblog(request):
-    form = ArticleForm()
+    form = ArticleForm(request.POST or None)
+
+    if form.is_valid():
+        article = form.save(commit = False)
+        article.author = request.user
+        article.save()
+
+        messages.success(request,"Blog is succesfully created.")
+        return redirect("index")
     
     return render(request,"addblog.html",{"form":form})

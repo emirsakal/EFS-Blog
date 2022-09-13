d819b4c4f11895e7a2e86426498ec6e48ebca2bd
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-13 19:02:11 +0300

    adding image/file

--- article/models.py
+++ article/models.py
@@ -6,5 +6,6 @@ class Article(models.Model):
     title = models.CharField(max_length = 50, verbose_name= "Title ")
     content = RichTextField()
     created_date = models.DateTimeField(auto_now_add=True, verbose_name= "Created Date")
+    article_image = models.FileField(blank = True,null = True,verbose_name="Image")
     def __str__(self):
         return self.title

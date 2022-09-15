c6de08dd6819bb3585694077ade0f5daeb38bd18
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2022-09-15 16:58:10 +0300

    commit

--- user/views.py
+++ user/views.py
@@ -9,49 +9,63 @@ from django.contrib.auth import login,authenticate,logout
 
 def register(request):
     
-    form = RegisterForm(request.POST or None)
-
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-
-        newUser = User(username = username)
-        newUser.set_password(password)
-
-        newUser.save()
-        login(request,newUser)
-        messages.success(request,"You have succesfully registered.")
-
+    if request.user.is_authenticated:
+        messages.info(request,"You can't login more than one account.")
         return redirect("index")
-    context = {
-            "form" : form
-        }
-    return render(request,"register.html",context)
+
+    else:
+        form = RegisterForm(request.POST or None)
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            newUser = User(username = username)
+            newUser.set_password(password)
+
+            newUser.save()
+            login(request,newUser)
+            messages.success(request,"You have succesfully registered.")
+
+            return redirect("index")
+        context = {
+                "form" : form
+            }
+        return render(request,"register.html",context)
 
 
 def loginUser(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form" : form
-    }
-
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-
-        user = authenticate(username = username,password = password)
-
-        if user is None:
-            messages.info(request,"Username or Password is wrong.")
-            return render(request,"login.html",context)
-        
-        messages.success(request,"You successfully logged in.")
-        login(request,user)
+    if request.user.is_authenticated:
+        messages.info(request,"You can't login more than one account.")
         return redirect("index")
+    
+    else:
+        form = LoginForm(request.POST or None)
+        context = {
+            "form" : form
+        }
 
-    return render(request,"login.html",context)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(username = username,password = password)
+
+            if user is None:
+                messages.info(request,"Username or Password is wrong.")
+                return render(request,"login.html",context)
+            
+            messages.success(request,"You successfully logged in.")
+            login(request,user)
+            return redirect("index")
+
+        return render(request,"login.html",context)
 
 def logoutUser(request):
-    logout(request)
-    messages.success(request,"You successfully logged out.")
-    return redirect("index")
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"You successfully logged out.")
+        return redirect("index")
+    else:
+        messages.info(request,"You can't logout without an acount.")
+        return redirect("index")
